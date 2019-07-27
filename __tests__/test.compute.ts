e3478f10d4291b6a14e4daf665e805c6b6e65d40
kmd2kmd <>
2019-07-27 13:39:30 +0900

    add eslint 非推奨なts-lintを廃止してes-lintを採用

--- __tests__/test.compute.ts
+++ __tests__/test.compute.ts
@@ -1,31 +1,31 @@
-import {App, Stack} from "@aws-cdk/core"
-import {SynthUtils} from "@aws-cdk/assert"
-import {ComputeStack} from '../lib/compute-stack'
+import {App, Stack} from "@aws-cdk/core";
+import {SynthUtils} from "@aws-cdk/assert";
+import {ComputeStack} from "../lib/compute-stack";
 import {SecurityGroup, Vpc} from "@aws-cdk/aws-ec2";
 import {Role, ServicePrincipal} from "@aws-cdk/aws-iam";
 
 describe("compute", () => {
-    test("default", () => {
-        const app = new App();
-        app.node.setContext("prj", "ssprj");
-        app.node.setContext("stage", "ssstage");
-        app.node.setContext("ssstage", {
-            "nat_gateways": 1
-        });
-        const givenStack = new Stack(app, 'GivenStack');
-        const instanceRole = new Role(givenStack, 'TestRole', {
-            roleName: "test",
-            assumedBy: new ServicePrincipal('ec2.amazonaws.com'),
-        });
-        const vpc = new Vpc(givenStack, 'TestVPC');
-        const sg = new SecurityGroup(givenStack, 'TestSecurityGroup', {
-            vpc: vpc,
-        });
-        const stack = new ComputeStack(app, 'TestComputeStack', {
-            vpc: vpc,
-            instanceRole: instanceRole,
-            appSg: sg
-        });
-        expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot()
-    })
+  test("default", () => {
+    const app = new App();
+    app.node.setContext("prj", "ssprj");
+    app.node.setContext("stage", "ssstage");
+    app.node.setContext("ssstage", {
+      nat_gateways: 1
+    });
+    const givenStack = new Stack(app, "GivenStack");
+    const instanceRole = new Role(givenStack, "TestRole", {
+      roleName: "test",
+      assumedBy: new ServicePrincipal("ec2.amazonaws.com")
+    });
+    const vpc = new Vpc(givenStack, "TestVPC");
+    const sg = new SecurityGroup(givenStack, "TestSecurityGroup", {
+      vpc: vpc
+    });
+    const stack = new ComputeStack(app, "TestComputeStack", {
+      vpc: vpc,
+      instanceRole: instanceRole,
+      appSg: sg
+    });
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+  });
 });
--- __tests__/test.network.ts
+++ __tests__/test.network.ts
@@ -1,16 +1,16 @@
-import {App} from "@aws-cdk/core"
-import {SynthUtils} from "@aws-cdk/assert"
-import {NetworkStack} from '../lib/network-stack'
+import {App} from "@aws-cdk/core";
+import {SynthUtils} from "@aws-cdk/assert";
+import {NetworkStack} from "../lib/network-stack";
 
 describe("network", () => {
-    test("default", () => {
-        const app = new App();
-        app.node.setContext("prj", "ssprj");
-        app.node.setContext("stage", "ssstage");
-        app.node.setContext("ssstage", {
-            "nat_gateways": 1
-        });
-        const stack = new NetworkStack(app, 'TestNetworkStack');
-        expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot()
-    })
+  test("default", () => {
+    const app = new App();
+    app.node.setContext("prj", "ssprj");
+    app.node.setContext("stage", "ssstage");
+    app.node.setContext("ssstage", {
+      nat_gateways: 1
+    });
+    const stack = new NetworkStack(app, "TestNetworkStack");
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+  });
 });
